7e34e999bba454554d08985866c3c11d61b0c144
saituumi <sayana.kirk@gmail.com>
2025-03-29 17:18:12 -0700

    Added detailed description to the function

--- src/check_missing_value.py
+++ src/check_missing_value.py
@@ -2,12 +2,23 @@ import click
 import pandas as pd 
 
 def check_missing_value(bill_data):
+        
+    """
+    Checks if the data haas missing (NAN) values
+    
+    Parameters:
+        “bill_data: a variable that stores a pandas DataFrame — which is a data table loaded from a CSV file.
+    
+    Output:
+       If the dataset has NAN values, function prints a message in the terminal and filters the data to remove the NAN values
+
+    """
+        
     # Check for missing values
     missing_values = bill_data.isna().sum().sum()
 
     if missing_values:
         click.echo(f"Found {missing_values} missing values in the data.")
-
         # Drop missing values
         bill_data.dropna(inplace=True)
         click.echo("Missing values dropped!")
